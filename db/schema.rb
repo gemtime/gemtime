4988bf94e1fb318ec0f376ce0a4bd392da784de5
Jon Moss <me@jonathanmoss.me>
2014-12-23 20:02:04 -0500

    Migrated posts scaffold migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141224003403) do
+ActiveRecord::Schema.define(version: 20141224005956) do
+
+  create_table "posts", force: true do |t|
+    t.string   "name"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
