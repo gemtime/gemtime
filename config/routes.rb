c4bb9ec060f6d23b0227143732c5f2c1f2ac0466
Jon Moss <me@jonathanmoss.me>
2014-12-23 19:41:26 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
-  get 'pages/home'
 
-  get 'pages/about'
+  # Root
+  root to: 'pages/home'
 
+  # Pages
+  get '/about' => 'pages#about'
+
+  # User Authentication
   devise_for :users
+
+
+
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
