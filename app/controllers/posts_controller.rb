a6dda366d3819d6048b3e212c6fea501b84a3ceb
Jon Moss <me@jonathanmoss.me>
2014-12-23 20:09:27 -0500

    Bootstrap-ified Posts views and deprecated json builders in controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,39 +1,53 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-  respond_to :html
-
+  # GET /posts
   def index
     @posts = Post.all
-    respond_with(@posts)
   end
 
+  # GET /posts/1
   def show
-    respond_with(@post)
   end
 
+  # GET /posts/new
   def new
     @post = Post.new
-    respond_with(@post)
   end
 
+  # GET /posts/1/edit
   def edit
   end
 
+  # POST /posts
   def create
     @post = Post.new(post_params)
-    @post.save
-    respond_with(@post)
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
   end
 
+  # PATCH/PUT /posts/1
   def update
-    @post.update(post_params)
-    respond_with(@post)
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
   end
 
+  # DELETE /posts/1
   def destroy
     @post.destroy
-    respond_with(@post)
+    respond_to do |format|
+      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+    end
   end
 
   private
